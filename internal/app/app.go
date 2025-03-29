fd6d997049567b94253033bad2b5a2e739d51250
elegant <illds@mail.ru>
2025-03-29 23:53:58 +0300

    list parking lots merge

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,8 +2,10 @@ package app
 
 import (
 	grpcapp "cult/internal/app/grpc"
+	parking_lot2 "cult/internal/repository/parking_lot"
 	"cult/internal/repository/user_repository"
 	"cult/internal/services/auth"
+	"cult/internal/services/parking_lot"
 	"github.com/jackc/pgx/v5"
 	"log/slog"
 	"time"
@@ -18,10 +20,12 @@ func New(
 	conn *pgx.Conn, tokenTTL time.Duration, secret string) *App {
 
 	userRepository := user_repository.NewUserRepository(conn, log)
+	parkingLotRepository := parking_lot2.NewParkingLotRepository(conn, log)
 
 	authService := auth.New(log, userRepository, tokenTTL, secret)
+	parkingLotService := parking_lot.NewParkingLotService(log, parkingLotRepository)
 
-	grpcApp := grpcapp.New(log, authService, grpcPort)
+	grpcApp := grpcapp.New(log, authService, parkingLotService, grpcPort)
 
 	return &App{
 		GRPCServer: grpcApp,
---
+++ internal/grpc/auth/list_parking_lots.go
@@ -0,0 +1,42 @@
+package authgrpc
+
+import (
+	"context"
+	"cult/internal/domain"
+	sso "cult/pkg"
+	"fmt"
+	"github.com/samber/lo"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"strconv"
+)
+
+func (s *serverAPI) ListParkingLots(ctx context.Context, in *sso.ListParkingLotsRequest) (*sso.ListParkingLotsResponse, error) {
+	lots, err := s.parkingLot.GetAllParkingLots(ctx)
+	if err != nil {
+		return nil, status.Error(codes.Internal, fmt.Sprintf("failed to get list parking lots: %s", err.Error()))
+	}
+
+	return &sso.ListParkingLotsResponse{
+		ParkingLot: lo.Map(lots, func(item domain.ParkingLot, index int) *sso.ParkingLot {
+			return &sso.ParkingLot{
+				Number: int64(lo.Must(strconv.Atoi(item.ID))),
+				Type:   item.ParkingType.GetPBType(),
+				Status: getRandomParkingStatus(index),
+			}
+		}),
+		Total: int64(len(lots)),
+	}, nil
+}
+
+func getRandomParkingStatus(index int) sso.ParkingLotStatus {
+	switch index % 3 {
+	case 0:
+		return sso.ParkingLotStatus_AVAILABLE_PARKING_LOT_STATUS
+	case 1:
+		return sso.ParkingLotStatus_BOOKED_PARKING_LOT_STATUS
+	case 2:
+		return sso.ParkingLotStatus_BOOKED_BY_ME_PARKING_LOT_STATUS
+	}
+	panic("undefined parking lot status")
+}
